2861a9e22d39f68506f763fd337f399692987c08
redaphid <iam@aaronherres.com>
2017-07-30 15:53:25 -0700

    run the strategy

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const blackjack = require('engine-blackjack')
+const strategy = require('blackjack-strategy')
+const _ = require('lodash')
 const actions = blackjack.actions
 const Game = blackjack.Game
+function gameStateToStrategy(gameState) {
+  return [
+      _.map(gameState.handInfo.right.cards, 'value'),
+      gameState.dealerCards[0].value,
+      1,
+      false,
+      null
+    ]
+}
 
 const game = new Game()
-console.log(JSON.stringify(game.getState(), null, 2))
-
 game.dispatch(actions.deal())
+var gameState = game.getState()
+console.log(JSON.stringify(_.omit(gameState, 'deck'), null, 2))
 
-
-console.log(JSON.stringify(game.getState(), null, 2))
+var strategyOptions = gameStateToStrategy(gameState)
+console.log(strategyOptions)
+console.log(strategy.GetRecommendedPlayerAction.apply(this, strategyOptions))
